17beaf6c229a1dcd85389697cbcfce9e5f93c10b
Adam Kischinovsky <adam@MacBook-Pro-tilhrende-Adam.local>
2025-07-31 06:52:42 +0800

    added an about page for Meta approval

--- server.js
+++ server.js
@@ -325,6 +325,12 @@ function requireAnyUser(req, res, next) {
   }
 }
 
+// About page
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+
 // List bookings on dashboard
 
 
